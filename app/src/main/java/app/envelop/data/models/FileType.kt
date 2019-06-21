1350888bf483bc6c5e8d230f09fb682f142c196d
Sérgio Santos <me@sergiosantos.info>
2019-06-21 18:45:42 +0100

    New video icon. Add PDF icon.

--- app/src/main/java/app/envelop/data/models/FileType.kt
+++ app/src/main/java/app/envelop/data/models/FileType.kt
@@ -11,14 +11,16 @@ constructor(
   Image(R.drawable.ic_type_image),
   Audio(R.drawable.ic_type_audio),
   Video(R.drawable.ic_type_video),
-  Archive(R.drawable.ic_type_file);
+  Archive(R.drawable.ic_type_archive),
+  PDF(R.drawable.ic_type_pdf);
 
   companion object {
     private val MAPPING = mapOf(
       Image to arrayOf("png", "gif", "jpg", "jpeg", "svg", "tif", "tiff", "ico"),
       Audio to arrayOf("wav", "aac", "mp3", "oga", "weba", "midi"),
       Video to arrayOf("avi", "mpeg", "mpg", "mp4", "ogv", "webm", "3gp", "mov"),
-      Archive to arrayOf("zip", "rar", "tar", "gz", "7z", "bz", "bz2", "arc")
+      Archive to arrayOf("zip", "rar", "tar", "gz", "7z", "bz", "bz2", "arc"),
+      PDF to arrayOf("pdf")
     )
 
     fun fromContentType(type: String?) =
