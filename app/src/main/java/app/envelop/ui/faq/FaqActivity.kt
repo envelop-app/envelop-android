c850be3998ef2d1e71eca14b93a3a757b6955a90
Sergio Domingues <srgfdomingues@gmail.com>
2019-11-14 17:46:42 +0000

    FAQ screen implemented

--- app/src/main/java/app/envelop/ui/faq/FaqActivity.kt
+++ app/src/main/java/app/envelop/ui/faq/FaqActivity.kt
@@ -25,65 +25,51 @@ class FaqActivity : BaseActivity() {
     super.onCreate(savedInstanceState)
     component.inject(this)
     setContentView(R.layout.activity_faq)
-    toolbar.setTitle(R.string.faq_title)
     toolbar.enableNavigation()
-
     faq.settings.javaScriptEnabled = true
     faq.loadUrl(getString(R.string.faq_url))
-    faq.webChromeClient = object : WebChromeClient() {
-      override fun onProgressChanged(view: WebView?, newProgress: Int) {
-        progressBar.progress = newProgress
-        super.onProgressChanged(view, newProgress)
-      }
-    }
-    faq.webViewClient = object : WebViewClient() {
-      override fun onReceivedError(
-        view: WebView?,
-        request: WebResourceRequest?,
-        error: WebResourceError?
-      ) {
-        hadError = true
-        error?.let {
-          messageManager.showError(R.string.host_not_found)
-          super.onReceivedError(view, request, error)
-        }
-        faq.isVisible = false
-        progressBar.isVisible = false
-      }
-
-      override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-        val url = request?.url.toString()
-        return if (url.startsWith(getString(R.string.http)) || url.startsWith(getString(R.string.https))) {
-          openBrowser(request?.url)
-          true
-        } else if (url.startsWith(getString(R.string.mail_to))) {
-          openEmail(url.substring(url.indexOf(":") + 1))
-          true
-        } else
-          false
-      }
-
-      override fun onPageFinished(view: WebView?, url: String?) {
-        super.onPageFinished(view, url)
-        if (!hadError) {
-          faq.isVisible = true
-          progressBar.isVisible = true
-        }
-      }
-    }
-  }
-
-  private fun openEmail(email: String) {
-    val mailer = Intent(Intent.ACTION_SEND)
-    mailer.type = getString(R.string.text)
-    mailer.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
-    startActivity(Intent.createChooser(mailer, getString(R.string.send_email)))
+    faq.webChromeClient = CustomChromeClient()
+    faq.webViewClient = CustomWebViewClient()
   }
 
   private fun openBrowser(url: Uri?) {
     url?.let {
       val intent = Intent(Intent.ACTION_VIEW, url)
       startActivity(intent)
+    }
+  }
+
+  inner class CustomChromeClient : WebChromeClient() {
+    override fun onProgressChanged(view: WebView?, newProgress: Int) {
+      progressBar.progress = newProgress
+      super.onProgressChanged(view, newProgress)
+    }
+  }
+
+  inner class CustomWebViewClient : WebViewClient() {
+    override fun onReceivedError(
+      view: WebView,
+      request: WebResourceRequest,
+      error: WebResourceError
+    ) {
+      hadError = true
+      messageManager.showError(R.string.faq_host_not_found)
+      super.onReceivedError(view, request, error)
+      faq.isVisible = false
+      progressBar.isVisible = false
+    }
+
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+      openBrowser(request.url)
+      return true
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+      super.onPageFinished(view, url)
+      if (!hadError) {
+        faq.isVisible = true
+        progressBar.isVisible = true
+      }
     }
   }
 
