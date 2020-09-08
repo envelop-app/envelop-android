1b76b7ad962c19aef1af8f0e49a98e4ae0c973cd
FilipeA <filiperamosalves@gmail.com>
2020-09-08 11:13:26 +0100

    added: stopDownload test

--- app/src/test/java/app/envelop/domain/UploadServiceTest.kt
+++ app/src/test/java/app/envelop/domain/UploadServiceTest.kt
@@ -1,12 +1,14 @@
 package app.envelop.domain
 
 import app.envelop.common.FileHandler
+import app.envelop.common.Operation
 import app.envelop.common.Optional
 import app.envelop.data.models.Upload
 import app.envelop.data.models.UploadState
 import app.envelop.data.repositories.DocRepository
 import app.envelop.data.repositories.RemoteRepository
 import app.envelop.data.repositories.UploadRepository
+import app.envelop.data.security.Encrypter
 import app.envelop.data.security.EncrypterProvider
 import app.envelop.data.security.EncryptionKey
 import app.envelop.data.security.KeyGenerator
@@ -16,72 +18,88 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Flowable
 import io.reactivex.Observable
-import junit.framework.Assert.assertEquals
+import io.reactivex.Single
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class UploadServiceTest {
 
-    val remoteRepositoryMock = mock<RemoteRepository>()
-    val docRepositoryMock = mock<DocRepository>()
-    val uploadRepositoryMock = mock<UploadRepository>()
-    val fileHandlerMock = mock<FileHandler>()
-    val updateDocRemotelyMock = mock<UpdateDocRemotely>()
-    val encrypterProviderMock = mock<EncrypterProvider>()
-    val keyGeneratorMock = mock<KeyGenerator>()
+  private val remoteRepositoryMock = mock<RemoteRepository>()
+  private val docRepositoryMock = mock<DocRepository>()
+  private val uploadRepositoryMock = mock<UploadRepository>()
+  private val fileHandlerMock = mock<FileHandler>()
+  private val updateDocRemotelyMock = mock<UpdateDocRemotely>()
+  private val encrypterProviderMock = mock<EncrypterProvider>()
+  private val keyGeneratorMock = mock<KeyGenerator>()
 
-    val uploadService = UploadService(
-        remoteRepositoryMock,
-        docRepositoryMock,
-        uploadRepositoryMock,
-        fileHandlerMock,
-        updateDocRemotelyMock,
-        encrypterProviderMock,
-        keyGeneratorMock
-    )
+  private val uploadService = UploadService(
+    remoteRepositoryMock,
+    docRepositoryMock,
+    uploadRepositoryMock,
+    fileHandlerMock,
+    updateDocRemotelyMock,
+    encrypterProviderMock,
+    keyGeneratorMock
+  )
 
-    @Test
-    fun emptyUpload() {
-        whenever(uploadRepositoryMock.getAll()).thenReturn(Flowable.just(listOf()))
+  @Test
+  fun emptyUpload() {
+    whenever(uploadRepositoryMock.getAll()).thenReturn(Flowable.just(listOf()))
 
-        val stateStream = uploadService.state().test()
-        uploadService.startUpload()
+    val stateStream = uploadService.state().test()
+    uploadService.startUpload()
 
-        stateStream.assertValue(UploadState.Idle)
-    }
+    stateStream.assertValue(UploadState.Idle)
+  }
 
-    @Test
-    fun startUpload() {
-        val uploadFile = Upload(partSize = 1_000)
-        val fileDocument = DocFactory.fivePartsBuild()
+  @Test
+  fun startUpload() {
+    val uploadFile = Upload(partSize = 1_000)
+    val fileDocument = DocFactory.fivePartsBuild()
 
-        whenever(uploadRepositoryMock.getAll()).thenReturn(Flowable.just(listOf(uploadFile)))
-        whenever(docRepositoryMock.get(any())).thenReturn(
-            Observable.just(
-                Optional.create(
-                    fileDocument
-                )
-            )
-        )
-        whenever(
-            keyGeneratorMock.generate(
-                any(),
-                any()
-            )
-        ).thenReturn(EncryptionKey(key = ByteArray(0)))
-        whenever(fileHandlerMock.localFileToByteArray(any(), any(), any())).thenReturn(ByteArray(0))
-        whenever(uploadRepositoryMock.get(any())).thenReturn(Flowable.just(listOf(uploadFile)))
+    whenever(uploadRepositoryMock.getAll()).thenReturn(Flowable.just(listOf(uploadFile)))
+    whenever(docRepositoryMock.get(any())).thenReturn(Observable.just(Optional.create(fileDocument)))
+    whenever(keyGeneratorMock.generate(any(), any())).thenReturn(EncryptionKey(key = ByteArray(0)))
+    whenever(fileHandlerMock.localFileToByteArray(any(), any(), any())).thenReturn(ByteArray(0))
+    whenever(uploadRepositoryMock.get(any())).thenReturn(Flowable.just(listOf(uploadFile)))
 
-        val stateStream = uploadService.state().test()
-        uploadService.startUpload()
+    val stateStream = uploadService.state().test()
+    uploadService.startUpload()
 
-        val uploadValue = stateStream.values()[1]
+    val uploadValue = stateStream.values()[1]
 
-        assertThat(uploadValue, instanceOf(UploadState.Uploading::class.java))
-        val unwrappedUpload = uploadValue as UploadState.Uploading
+    assertThat(uploadValue, instanceOf(UploadState.Uploading::class.java))
+    val unwrappedUpload = uploadValue as UploadState.Uploading
 
-        assertEquals(unwrappedUpload.nextUpload.doc, fileDocument)
-        assertEquals(unwrappedUpload.nextUpload.upload, uploadFile)
-    }
+    assertEquals(unwrappedUpload.nextUpload.doc, fileDocument)
+    assertEquals(unwrappedUpload.nextUpload.upload, uploadFile)
+  }
+
+  @Test
+  fun stopUpload() {
+    val uploadFile = Upload(partSize = 1_000)
+    val fileDocument = DocFactory.fivePartsBuild()
+    val encryptedMock = mock<Encrypter>()
+
+    whenever(uploadRepositoryMock.getAll()).thenReturn(Flowable.just(listOf(uploadFile)))
+    whenever(docRepositoryMock.get(any())).thenReturn(Observable.just(Optional.create(fileDocument)))
+    whenever(keyGeneratorMock.generate(any(), any())).thenReturn(EncryptionKey(key = ByteArray(0)))
+    whenever(fileHandlerMock.localFileToByteArray(any(), any(), any())).thenReturn(ByteArray(0))
+    whenever(encrypterProviderMock.getOrError(any())).thenReturn(encryptedMock)
+    whenever(encryptedMock.encrypt(any(), any(), any())).thenReturn(Encrypter.Result(ByteArray(1)))
+    whenever(remoteRepositoryMock.uploadByteArray(any(), any(), any())).thenReturn(Single.never<Operation<Unit>>())
+
+    val stateStream = uploadService.state().test()
+    uploadService.startUpload()
+
+    val uploadValue = stateStream.values().last()
+    assertThat(uploadValue, instanceOf(UploadState.Uploading::class.java))
+
+    uploadService.stopUpload()
+
+    val stopDownload = stateStream.values().last()
+    assertThat(stopDownload, instanceOf(UploadState.Idle::class.java))
+  }
 }
